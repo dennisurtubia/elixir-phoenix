f7cd4f1646d82a9b7726aaf7dbdb0ad58bd36fe4
Dennis Urtubia <dennisurtubia@alunos.utfpr.edu.br>
2021-02-24 21:10:44 -0300

    first steps: database
    - Migrations
    - Schema

--- payment_api/config/config.exs
+++ payment_api/config/config.exs
@@ -18,6 +18,10 @@ config :payment_api, PaymentApiWeb.Endpoint,
   pubsub_server: PaymentApi.PubSub,
   live_view: [signing_salt: "VIbGiB1C"]
 
+config :payment_api, PaymentApi.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
